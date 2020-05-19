865feb4cc81d90778df8f66cf86cfeecb6df718c
yaseerhaja <yaseerhaja@gmail.com>
2020-05-19 10:41:34 +0200

    fixes

--- server/routes/api/importFile.js
+++ server/routes/api/importFile.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 const request = require("request");
 const multipart = require("connect-multiparty");
-const multipartMiddleware = multipart();
+const multipartMiddleware = multipart({ uploadDir: "./tmp/" });
 const csv = require("csv-parser");
 const fs = require("fs");
 
 const logger = require("../../logger");
 const API_END_POINT = process.env.API_END_POINT;
 
-var dir = "./tmp/";
+var dir = "./tmp";
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
 
 const replaceKeys = (object) => {
   Object.keys(object).forEach(function (key) {
@@ -26,28 +29,20 @@ const replaceKeys = (object) => {
 
 router.post("/csvFile", multipartMiddleware, (req, res) => {
   logger.log("info", "%s", req.originalUrl);
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-  }
-  var tmp_path = req.files.fileKey.path;
-  var target_path = "./tmp/" + req.files.fileKey.name;
-  //fs.rename(tmp_path, target_path, function (err) {
-    let data = [];
-    fs.createReadStream(target_path)
-      .pipe(csv())
-      .on("data", (row) => {
-        replaceKeys(row);
-        data.push(row);
-      })
-      .on("end", () => {
-        res.send(data);
-        console.log("CSV file successfully processed");
-      });
+  var file = req.files;
+  var target_path = file.fileKey.path;
 
-    // fs.unlink(tmp_path, function () {
-    //   if (err) throw err;
-    // });
-  //});
+  let data = [];
+  fs.createReadStream(target_path)
+    .pipe(csv())
+    .on("data", (row) => {
+      replaceKeys(row);
+      data.push(row);
+    })
+    .on("end", () => {
+      res.send(data);
+      console.log("CSV file successfully processed");
+    });
 });
 
 module.exports = router;
